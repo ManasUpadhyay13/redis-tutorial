cc345749cdb9b303a87cd7bcaa1307c2046062a7
Manas Upadhyay <manasupadhyay1318@gmail.com>
2024-01-14 03:45:26 +0530

    strings complete

--- client.js
+++ client.js
@@ -1,3 +1,14 @@
-const client = require("./index")
+const Redis = require('ioredis')
+const redis = new Redis()
 
 
+async function hello() {
+    const res = await redis.set("msg:1", "manas", "nx")
+    // const res = await redis.get("msg:1")
+    // await redis.expire("msg:1", 20)
+
+
+    // console.log(res);
+}
+
+hello()
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Redis } = require("ioredis")
 
 const client = new Redis()
 
-module.exports = { client }
+module.exports = client
 
 
 // we are tyring to create a server to hit the port of redis sever running on docker.
