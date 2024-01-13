ca6c23caddc3ed00a87467bb5e05125d0ec2ebfe
Manas Upadhyay <manasupadhyay1318@gmail.com>
2024-01-13 23:09:56 +0530

    index.js done

---
+++ index.js
@@ -0,0 +1,8 @@
+const { Redis } = require("ioredis")
+
+const client = new Redis()
+
+module.exports = { client }
+
+
+// we are tyring to create a server to hit the port of redis sever running on docker.
